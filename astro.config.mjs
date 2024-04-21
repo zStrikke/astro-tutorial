df36ff9f7005afb43380fe5e6ebadc5026139a8c
zStrikke <zstrikke@gmail.com>
2024-04-22 00:52:43 +0200

    added inslands and finished 1st part

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://astro-tutorial-zstrikke.netlify.app"
+  site: "https://astro-tutorial-zstrikke.netlify.app",
+  integrations: [preact()]
 });
